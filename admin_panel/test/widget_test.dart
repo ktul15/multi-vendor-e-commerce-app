d186ac33de7a7be6db131a6a93c98bc33382388b
Ketul Makwana <ketulm8@gmail.com>
2026-07-15 07:02:30 +0530

    test(admin): initialize dependencies in widget test

--- admin_panel/test/widget_test.dart
+++ admin_panel/test/widget_test.dart
@@ -1,10 +1,23 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:admin_panel/main.dart';
+import 'package:admin_panel/core/config/injection_container.dart';
+import 'package:admin_panel/features/auth/bloc/auth_cubit.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
+  setUpAll(() async {
+    TestWidgetsFlutterBinding.ensureInitialized();
+    SharedPreferences.setMockInitialValues({});
+    await sl.reset();
+    await initDependencies();
+    await sl<AuthCubit>().checkAuth();
+  });
+
   testWidgets('App should render', (WidgetTester tester) async {
     await tester.pumpWidget(const AdminPanelApp());
+    await tester.pumpAndSettle();
 
-    expect(find.text('Admin Panel'), findsOneWidget);
+    expect(find.text('Admin Login'), findsOneWidget);
+    expect(find.text('Sign In'), findsOneWidget);
   });
 }
