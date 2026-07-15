1099ddb8bdd52f377231bab6db2815ea7bca6ca6
Ketul Makwana <ketulm8@gmail.com>
2026-07-15 07:01:48 +0530

    chore(backend): resolve lint configuration issues

--- backend/src/modules/category/category.controller.ts
+++ backend/src/modules/category/category.controller.ts
@@ -1,4 +1,4 @@
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response } from 'express';
 import { CategoryService } from './category.service';
 import catchAsync from '../../utils/catchAsync';
 import { ApiResponse } from '../../utils/apiResponse';
